64cee5584448c91f4c6f79c2270d4847ef8b94ce
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-10-22 21:46:30 +0600

    add user config

---
+++ src/modules/users/user.validation.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const userValidation = z.object({
+  name: z.string().min(2, "Name must be at least 2 character"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export { userValidation };
